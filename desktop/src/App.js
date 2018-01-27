05a34af73e6a876311967e4b165d5e8971c9ee93
iamdudeman <timothy.solum@gmail.com>
2018-01-27 17:37:42 -0600

    desktop stuff working again

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { containers, store } from 'shared';
+import { containers, store } from '../../shared';
 import Counter from './components/Counter';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
